fb35ff33bd0284682772b7d6f7e387c4c1e2f425
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-04-24 20:23:47 -0500

    working on answer validator

---
+++ collecta/answers.go
@@ -0,0 +1,106 @@
+package core
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// AnswerKind try to give a name to the answer
+type AnswerKind string
+
+const Satisfaction AnswerKind = "satisfaction"
+const Option AnswerKind = "option"
+const Text AnswerKind = "text"
+const Boolean AnswerKind = "boolean"
+
+// // AnswerValidation only wraps the necessary info to understand an answer
+// type AnswerValidation struct {
+// 	Valid bool
+// 	Multiple bool
+// }
+
+func validate(input []string, kind AnswerKind, options ...map[string]string) (bool, bool, error) {
+	if len(input) == 0 {
+		return false, false, errors.New("invalid answers length, please add one answer at lest")
+	}
+
+	for _, in := range input {
+		in = strings.TrimSpace(in)
+		switch kind {
+		case Satisfaction:
+			if _, err := strconv.ParseFloat(in, 64); err != nil {
+				return false, false, errors.Wrap(err, "invalid response, that should be a decimal number <0,1> (e.g. 0.4)")
+			}
+		case Text:
+			if in == "" {
+				return false, false, errors.New("void answer")
+			}
+		case Option:
+			if len(options) == 0 {
+				return false, false, errors.New("please add the alteratives as a map[string]string")
+			}
+			keys := make([]string, 0)
+			values := make([]string, 0)
+			for k, v := range options[0] {
+				keys = append(keys, strings.TrimSpace(k))
+				values = append(values, strings.TrimSpace(v))
+			}
+
+			ok1, ok2 := true, true
+			if !strings.Contains(strings.Join(keys, " "), strings.TrimSpace(in)) {
+				ok1 = false
+			}
+			if !strings.Contains(strings.Join(values, " "), strings.TrimSpace(in)) {
+				ok2 = false
+			}
+
+			if !ok1 && !ok2 {
+				return false, false, errors.New("option not found on the available values")
+			}
+		case Boolean:
+			if _, err := strconv.ParseBool(in); err != nil {
+				if !strings.Contains("yes no si no", in) {
+					return false, false, errors.New("invalid boolean answer")
+				}
+			}
+		default:
+			return false, false, errors.New("invalid kind of answer, are valid: satisfaction|text|option|boolean")
+		}
+	}
+
+	m := false
+	if len(input) > 1 {
+		m = true
+	}
+
+	return true, m, nil
+}
+
+func answerIsSatisfaction(input []string, acceptMultiple ...bool) (bool, error) {
+	valid, multiple, err := validate(input, Satisfaction)
+	if err != nil {
+		return false, errors.Wrap(err, "error at validate your input")
+	}
+
+	if len(acceptMultiple) == 0 && multiple {
+		return false, nil
+	}
+
+
+}
+
+func answerIsOption(input []string, options map[string]string, acceptMultiple ...bool) (bool, error) {
+
+}
+
+func answerIsBoolean(input []string, acceptMultiple ...bool) (bool, error) {
+
+}
+
+func answerIsText(input []string, acceptMultiple ...bool) (bool, error) {
+
+}
+
+
