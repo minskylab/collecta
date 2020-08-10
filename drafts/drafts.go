09ecf55caf2b1b216cd8a2799c95d2ec03e270cd
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-08-09 21:10:19 -0500

    added typo fix

--- drafts/drafts.go
+++ drafts/drafts.go
@@ -8,6 +8,7 @@ import (
 	"github.com/minskylab/collecta/uuid"
 )
 
+// GenerateUTECDemo generates a simple test survey
 func GenerateUTECDemo(ctx context.Context, collectaDB *db.DB, domainID uuid.UUID, userID uuid.UUID) (*ent.Survey, error) {
 	return generateUTECDemoSurvey(ctx, collectaDB, domainID, userID)
 }
